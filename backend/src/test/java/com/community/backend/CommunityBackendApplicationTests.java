70b1b527a294c8d487b3c262deacb8811bca1ddb
hoonie-dvp <sihun457@gmail.com>
2026-02-08 18:03:44 +0000

    First Commit : Folder structure

---
+++ backend/src/test/java/com/community/backend/CommunityBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.community.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
