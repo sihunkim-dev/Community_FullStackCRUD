e6f4780c0fbd366289146974bd9e8421553428af
hoonie-dvp <sihun457@gmail.com>
2026-02-09 02:07:36 +0000

    SET : proxy set 3000

--- backend/src/main/java/com/community/backend/CommunityBackendApplication.java
+++ backend/src/main/java/com/community/backend/CommunityBackendApplication.java
@@ -2,9 +2,9 @@ package com.community.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
-public class CommunityBackendApplication {
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})public class CommunityBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommunityBackendApplication.class, args);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useEffect, useState} from "react";
+
+import api from "./axiosConfig.js";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const[testMessage, setTestMessage] = useState("Loading...");
 
+    useEffect(() => {
+        api.get('/test')
+            .then((response) => {
+                console.log("Loaded data:",response.data);
+                setTestMessage(response.data);
+            })
+        .catch((error) => {
+            console.log(error);
+        })
+    })
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1>프론트엔드 - 백엔드 통신 테스트</h1>
+            <hr />
+            {/* 백엔드에서 온 메시지가 여기 떠야 성공! */}
+            <h2 style={{ color: 'blue' }}>{testMessage}</h2>
+        </div>
     </>
   )
 }
